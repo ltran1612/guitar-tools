ef540b3956c45c278b8c197abe392873e0fce373
Long Tran <longdtranw@gmail.com>
2025-06-25 21:53:28 -0600

    setup  for firebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './tuner.js',
+  entry: './src/tuner.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
 };
